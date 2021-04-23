59c42d15a4ff044d184f73e49935b6c963064915
Christopher Granade <cgranade@cgranade.com>
2021-04-23 02:44:36 +0000

    Set pathPrefix.

--- .eleventy.js
+++ .eleventy.js
@@ -117,7 +117,7 @@ module.exports = function(eleventyConfig) {
     // You can also pass this in on the command line using `--pathprefix`
 
     // Optional (default is shown)
-    pathPrefix: "/",
+    pathPrefix: "/camera-ergo-sum",
     // -----------------------------------------------------------------
 
     // Pre-process *.md files with: (default: `liquid`)
